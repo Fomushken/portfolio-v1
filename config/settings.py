65455c6044cdca969d817eb0cd04bf92a2090bf1
Marat Fomin <skinner.fomin@gmail.com>
2024-07-24 20:17:53 +0400

    trial #999999

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,8 @@ DEBUG = True
 ALLOWED_HOSTS = ['web-production-cd9a.up.railway.app',
                  'fomushken.up.railway.app',
                  'www.fomushken.up.railway.app',
-                 '127.0.0.1'
+                 '127.0.0.1',
+                 'localhost'
                  ]
 
 CSRF_TRUSTED_ORIGINS = [
